e3b3809b0f820d191b03c40d8a262c8a9926c7c5
Alex Gorelick <agorelick@mgh.harvard.edu>
2024-07-29 00:00:26 -0400

    adding script

---
+++ R/process_scna_data.R
@@ -0,0 +1,7 @@
+source('R/process_scna_C146.R')
+source('R/process_scna_C154.R')
+source('R/process_scna_C157.R')
+source('R/process_scna_C159.R')
+source('R/process_scna_C161.R')
+source('R/process_scna_C186.R')
+
